337d007d47e45893371707a12cdcc99069b714a4
ShepLT1 <luke.t.shepherd@gmail.com>
2023-04-10 23:09:53 -0700

    Remove redundant default model parameters. Remove condition model enums

--- individual_collections/models.py
+++ individual_collections/models.py
@@ -6,7 +6,7 @@ from hobbies.models import Item, Hobby
 
 
 class Collection(BaseModel):
-    name = models.CharField(null=False, max_length=64, default=None)
+    name = models.CharField(max_length=64, default=None)
 
     hobby = models.ForeignKey(
         Hobby, on_delete=models.CASCADE, related_name="collection_hobby", default=1
@@ -21,22 +21,22 @@ class Collection(BaseModel):
 
 
 class Condition(BaseModel):
-    class ConditionChoices(models.TextChoices):
-        NEW = "NE", _("New")
-        MINT = "MT", _("Mint")
-        LIKENEW = "LN", _("Used (Like new)")
-        NEARMINT = "NM", _("Used (Near mint)")
-        EXCELLENT = "EX", _("Used (Excellent)")
-        VERYGOOD = "VG", _("Used (Very good)")
-        GOOD = "GD", _("Used (Good)")
-        LIGHTPLAYED = "LP", _("Used (Lightly played)")
-        PLAYED = "PL", _("Used (Played)")
-        POOR = "PR", _("Used (Poor)")
-        OPEN = "OB", _("Used (Open box)")
 
-    name = models.CharField(
-        max_length=2, null=False, blank=False, choices=ConditionChoices.choices
-    )
+    # TODO: add these conditions as records, not ENUMs
+    # class ConditionChoices(models.TextChoices):
+    #     NEW = "NE", _("New")
+    #     MINT = "MT", _("Mint")
+    #     LIKENEW = "LN", _("Used (Like new)")
+    #     NEARMINT = "NM", _("Used (Near mint)")
+    #     EXCELLENT = "EX", _("Used (Excellent)")
+    #     VERYGOOD = "VG", _("Used (Very good)")
+    #     GOOD = "GD", _("Used (Good)")
+    #     LIGHTPLAYED = "LP", _("Used (Lightly played)")
+    #     PLAYED = "PL", _("Used (Played)")
+    #     POOR = "PR", _("Used (Poor)")
+    #     OPEN = "OB", _("Used (Open box)")
+
+    name = models.CharField(unique=True, max_length=64, default=None)
 
     hobby = models.ManyToManyField(Hobby, related_name="condition_hobby", default=[1])
 
