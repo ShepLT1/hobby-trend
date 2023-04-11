3e6609766fc4f885b1d0badcf21e9e443cdc6de8
ShepLT1 <luke.t.shepherd@gmail.com>
2023-04-10 17:45:26 -0700

    Update ListingsView serializer and add url

--- hobbies/urls.py
+++ hobbies/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("<uuid:hobby_id>", views.HobbyView.as_view()),
     path("items/", views.ItemsView.as_view({"get": "list"})),
     path("items/<uuid:item_id>", views.ItemView.as_view()),
+    path("items/<uuid:item_id>/listings", views.ListingsView.as_view()),
 ]
