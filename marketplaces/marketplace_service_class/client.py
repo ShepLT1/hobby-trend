8d5773ed2d17f1c9ff80abfd326aad7189870752
ShepLT1 <luke.t.shepherd@gmail.com>
2023-06-06 14:14:53 -0700

    Rename api service class

---
+++ marketplaces/marketplace_service_class/client.py
@@ -0,0 +1,38 @@
+import requests
+from hobbies.models import (
+    Hobby,
+    Marketplace,
+    Variation,
+    Format,
+    Set,
+    Item,
+    Listing,
+    Media,
+    MarketplaceItem,
+)
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from data_cottage.settings import env
+import json
+
+
+class Marketplace:
+    def __init__(self, client):
+        self.client = client
+
+    def get_item(self, item, marketplace_item):
+        marketplace_item = MarketplaceItem.objects.get(
+            item=item,
+            listing_source=Marketplace.objects.get(name=self.client.source_name),
+        )
+        if marketplace_item.data["id"] == None:
+            response = self.client.initialize_item(item, marketplace_item)
+        else:
+            response = self.client.get_item_by_id(marketplace_item.data["id"])
+        return response
+
+    def create_listing(self, item):
+        marketplace_item = self.get_item(item)
+        marketplace = Marketplace.objects.get(name=self.client.source_name)
+        response = self.client.create_listing(item, marketplace, marketplace_item)
+        return response
