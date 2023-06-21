9a2673b8b467a75006eb8dd9b6b5452f20cdfb4a
ShepLT1 <luke.t.shepherd@gmail.com>
2023-06-21 15:39:37 -0700

    Update Marketplace wrapper reference and marketplace retrieval method

--- marketplaces/ingest.py
+++ marketplaces/ingest.py
@@ -11,17 +11,20 @@ from hobbies.models import (
 )
 from marketplaces.ebay.client import Ebay
 from marketplaces.tcgplayer.client import TCGPlayer
-from marketplaces.marketplace_service_class.client import Marketplace
+from marketplaces.marketplace_service_class.client import MarketplaceWrapper
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
 
 
 def ingest_listings_by_hobby(hobby):
     marketplace_client_dict = {"ebay": Ebay(), "tcgplayer": TCGPlayer()}
-    items = Item.objects.filter(hobby_id=getattr(hobby, "id"))
-    marketplaces = Marketplace.objects.filter(hobby=hobby)
+    items = Item.objects.filter(hobby__id=getattr(hobby, "id"))
+    marketplaces = Marketplace.objects.filter(hobby__id=getattr(hobby, "id"))
     marketplace_clients = []
     for marketplace in marketplaces:
         marketplace_clients.append(marketplace_client_dict[marketplace.name.lower()])
     for item in items:
         for marketplace_client in marketplace_clients:
-            marketplace = Marketplace(marketplace_client)
+            marketplace = MarketplaceWrapper(marketplace_client)
             marketplace.ingest_listing(item)
